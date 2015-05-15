e9ef98af16442daeb30a117ed43007931cbb7e2a
Aron Cedercrantz <aron@cedercrantz.se>
2015-05-15 19:38:19 +0200

    Add basic Xcode project

---
+++ Sources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS Template
+//
+//  Created by Aron Cedercrantz on 15/05/15.
+//  Copyright (c) 2015 Unify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sources/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS Template
+//
+//  Created by Aron Cedercrantz on 15/05/15.
+//  Copyright (c) 2015 Unify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
